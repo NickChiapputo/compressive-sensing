c74046c6e8badd848633d05ab05244bc10346ff0
NickChiapputo <chiapputo.nick@gmail.com>
2021-08-10 09:11:34 -0500

    Began implementation of OMP.

--- matching_pursuit/omp.py
+++ matching_pursuit/omp.py
@@ -7,14 +7,34 @@ from util import dct2
 
 from imageio import imwrite
 
-def omp( y, dictionary ):
+def omp( A, b, K, N, M ):
 	# System parameters
-	print( f'{dictionary.shape = }' )
+	residual = b		# r_0 = b
+	x = np.empty( N, dtype='float64' )
+	Lambda = np.empty( K )
+	A_k = np.empty( (M,1) )
 
-	# Initialization
-	residual = y 		# r_0 = b
-	# estimated_signal = np.zeros(  )	# x_tilde = ∅
-	# atoms = 
+	M_range = np.arange( M )
+
+
+	for k in range( 0, K ):
+		print( f"\rIteration {k}", end = '' )
+
+		max_val = 0
+		max_ind = 0
+
+		for i in np.setdiff1d( M_range, Lambda ):
+			tmp = np.transpose( A[ :, i ] ) @ residual / np.linalg.norm( A[ :, i ] )
+			if abs( tmp ) > max_val:
+				max_val = abs( tmp )
+				max_ind = i
+
+		Lambda[ k ] = max_ind
+		print( f"\n{A_k.shape}" )
+		A_k = np.hstack( ( A_k, A[ :, max_ind ] ) )
+		print( f"\n{A_k.shape}" )
+		x_ls = np.linalg.pinv( A_k ) @ b
+		residual = b - A_k @ x_ls
 
 
 	# Calculate the column in the sensing matrix that has the 
@@ -27,6 +47,10 @@ def omp_cs( img, rows, cols, n_channels, sample_percentages ):
 	psnr_results = np.zeros( len( sample_percentages ) )
 
 	A = dct2( np.identity( rows * cols ) )
+
+
+	# Threshold number of coefficients
+	K = 100
 
 
 	# Iterate through each sample percentage value.
@@ -42,4 +66,4 @@ def omp_cs( img, rows, cols, n_channels, sample_percentages ):
 
 		# Iterate through and reconstruct at each channel of the image.
 		for j in range( n_channels ):
-			opm( y[ :, j ], A_i )
+			omp( A_i, y[ :, j ], K, cols, rows )
